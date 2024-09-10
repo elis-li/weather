e3b319529b83135665bfe76b6b1def2eeddf966a
elis-li <petrushina.2014@mail.ru>
2024-09-10 14:24:50 +0300

    1

--- lib/weather.dart
+++ lib/weather.dart
@@ -1,3 +1,7 @@
+
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
 class Weather{
   static const String weatherURL =  "http://openweathermap.org/img/w/";
 
@@ -10,4 +14,30 @@ class Weather{
     return weatherURL + iconURL + ".png";
   }
   Weather (this.dateTime, this.degree, this.clouds, this.iconURL);
+}
+
+class WeatherListItem extends StatelessWidget{
+  static var _dateFormat = DateFormat('yyyy-MM-dd - hh:mm');
+
+  final Weather weather;
+
+  WeatherListItem(this.weather);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(children: [
+        Expanded(
+          flex: 3,
+          child: Text(_dateFormat.format(weather.dateTime))),
+        Expanded(
+          flex: 1,
+          child: Text(weather.degree.toString() + " C"),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image.network(weather.getIconURL()))
+      ]));
+  }
 }
